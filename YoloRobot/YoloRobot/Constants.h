4f8a24ad39c70e0f3a49c08f284119b4b933b24d
Ariel Rodriguez <ariel@volonbolon.net>
2015-02-10 21:47:10 -0300

    Add constantas and content view controller

---
+++ YoloRobot/YoloRobot/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  YoloRobot
+//
+//  Created by Ariel Rodriguez on 2/10/15.
+//  Copyright (c) 2015 Ariel Rodriguez. All rights reserved.
+//
+
+#ifndef YoloRobot_Constants_h
+#define YoloRobot_Constants_h
+
+NSString *const kSelectedBundleNameKey = @"kSelectedBundleNameKey";
+
+#endif
