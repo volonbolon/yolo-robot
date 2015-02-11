8c5b40df23f407dd7132ab0ca889187f99cc0f50
Ariel Rodriguez <ariel@volonbolon.net>
2015-02-11 00:03:32 -0300

    Add helpers category to UIColor

---
+++ YoloRobot/YoloRobot/UIColor+Helpers.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+Helpers.h
+//  YoloRobot
+//
+//  Created by Ariel Rodriguez on 2/10/15.
+//  Copyright (c) 2015 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Helpers)
++ (UIColor *)colorWithHexString:(NSString *)hexString;
+@end
